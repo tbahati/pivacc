3e317dd045724cfc069e8a2d4f881efa6c4184bc
bahati <tuyijean2003@gmail.com>
2022-02-05 23:51:21 +0200

    first project

---
+++ src/main/java/rw/tb/pivacc/PivaccApplication.java
@@ -0,0 +1,13 @@
+package rw.tb.pivacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivaccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PivaccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/tb/pivacc/PivaccApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.tb.pivacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivaccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
